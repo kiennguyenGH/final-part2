4a2f15e67a5c5d9bc0e118fd6a9034bc198634f5
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-07 11:52:43 +0000

    Step 16: Add timeout for read()

--- kilo.c
+++ kilo.c
@@ -21,15 +21,19 @@ void enableRawMode()
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
     enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while (1)
     {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
@@ -38,6 +42,7 @@ int main()
         {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q') break;
     }
     return 0;
 }
