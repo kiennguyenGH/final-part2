081de9e2f0a8f7724d7b15fdcd19aa3d785be8c2
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-08 08:45:21 +0000

    Step 73: Update editor scroll for horizontal

--- kilo.c
+++ kilo.c
@@ -253,6 +253,14 @@ void editorScroll()
     {
         E.rowoff = E.cy - E.screenrows + 1;
     }
+    if (E.cx < E.coloff)
+    {
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols)
+    {
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 void editorDrawRows(struct abuf *ab)
