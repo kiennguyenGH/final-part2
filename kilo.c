d8adeb9a6450371e7854e9b7ca49b02353b3f29d
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-08 06:22:21 +0000

    More bug fixes

--- kilo.c
+++ kilo.c
@@ -48,7 +48,7 @@ void enableRawMode()
     struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
-    raw.c_cflag &= (CS8);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
