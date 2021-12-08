995a07a70d35cec5fa69f3450855207e991eeb23
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-08 02:38:52 +0000

    Step 27: Windows size

--- kilo.c
+++ kilo.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <errno.h>
-
+#include <sys/ioctl.h>
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -59,6 +59,22 @@ char editorReadKey()
         if (nread == -1 && errno != EAGAIN) die("read");
     }
     return c;
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+
 }
 
 /*** output ***/
