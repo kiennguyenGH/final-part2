4e23e4a167cfc5d2f96c24ec3eef8229605d636e
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-08 02:22:25 +0000

    Step 22: Clear screen

--- kilo.c
+++ kilo.c
@@ -54,6 +54,12 @@ char editorReadKey()
     return c;
 }
 
+/*** output ***/
+void editorRefreshScreen()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
 /*** input ***/
 void editorProcessKeypress()
 {
@@ -72,6 +78,7 @@ int main()
     enableRawMode();
     while (1)
     {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
     return 0;
