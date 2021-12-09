538328731749cccb80dff2e5015763fbb26dc27c
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-09 00:48:54 +0000

    Step 88: Cx to rx

--- kilo.c
+++ kilo.c
@@ -191,6 +191,21 @@ int getWindowSize(int *rows, int *cols)
 
 /*** row operations ***/
 
+void editorRowCxToRx(erow *row, int cx)
+{
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++)
+    {
+        if (row->chars[j] == '\t')
+        {
+            rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+        }
+        rx++;
+    }
+    return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
     int tabs = 0;
