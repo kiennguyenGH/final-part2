d9674b5028f9b95fb9949fa409031210c8354d1d
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-09 00:29:40 +0000

    Step 82: Use render

--- kilo.c
+++ kilo.c
@@ -317,10 +317,10 @@ void editorDrawRows(struct abuf *ab)
         }
         else
         {
-            int len = E.row[filerow].size - E.coloff;
+            int len = E.row[filerow].rsize - E.coloff;
             if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+            abAppend(ab, &E.row[filerow].render[E.coloff], len);
         }
         abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1)
