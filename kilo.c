93eb0a64db070c9ad0af953c88b184e54f31a217
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-09 00:41:15 +0000

    Step 85: Add rx

--- kilo.c
+++ kilo.c
@@ -43,6 +43,7 @@ typedef struct erow
 struct editorConfig
 {
     int cx, cy;
+    int rx;
     int rowoff;
     int coloff;
     int screenrows;
@@ -458,6 +459,7 @@ void initEditor()
 {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.rowoff = 0;
     E.coloff = 0;
     E.numrows = 0;
