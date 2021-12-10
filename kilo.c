f76724e26cc55c066565d90c8246a5b286b4ba99
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-10 04:48:17 +0000

    Step 115: Add quit confirmation

--- kilo.c
+++ kilo.c
@@ -21,6 +21,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -494,15 +495,18 @@ void editorMoveCursor(int key) {
 
 void editorProcessKeypress() {
     int c = editorReadKey();
+    static int quit_times = KILO_QUIT_TIMES;
 
     switch (c) {
         case '\r':
             //To do
             break;
         case CTRL_KEY('q'):
-            write(STDOUT_FILENO, "\x1b[2J", 4);
-            write(STDOUT_FILENO, "\x1b[H", 3);
-            exit(0);
+            if (E.dirty && quit_times > 0) {
+                editorSetStatusMessage("WARNING!!! File has unsaved changes. Oress Ctrl-Q %d more times to quit.", quit_times);
+                quit_times--;
+                return;
+            }
             break;
 
         case CTRL_KEY('s'):
@@ -555,6 +559,8 @@ void editorProcessKeypress() {
             editorInsertChar(c);
             break;
     }
+
+    quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
