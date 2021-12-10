6d3a4173245aec98dddd1ac5d52cb5758fef3588
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-10 05:01:24 +0000

    Step 118: Keypresses delete char

--- kilo.c
+++ kilo.c
@@ -548,7 +548,8 @@ void editorProcessKeypress() {
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
-            //To do
+            if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+            editorDelChar();
             break;
 
         case PAGE_UP:
