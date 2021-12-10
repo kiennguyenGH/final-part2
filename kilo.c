f25e1a7659c8e0c9b642cc30477191c6d0d3ca90
kiennguyenGH <ktnguyen4647@gmail.com>
2021-12-10 04:59:53 +0000

    Step 117: Implement delete char

--- kilo.c
+++ kilo.c
@@ -251,12 +251,23 @@ void editorRowDelChar(erow *row, int at) {
 }
 
 /*** editor operations ***/
+
 void editorInsertChar(int c) {
     if (E.cy == E.numrows) {
         editorAppendRow("", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorDelChar() {
+    if (E.cy == E.numrows) return;
+
+    erow *row = &E.row[E.cy];
+    if (E.cx > 0) {
+        editorRowDelChar(row, E.cx - 1);
+        E.cx--;
+    }
 }
 
 /*** file i/o ***/
